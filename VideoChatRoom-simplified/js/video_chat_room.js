a413ebc3d513466e2f48cdbedd68e5efd849ef87
Daniel Gustavsson <danielgu@kth.se>
2016-11-29 16:56:43 +0100

    serve as SSL/TSL

--- VideoChatRoom-simplified/js/video_chat_room.js
+++ VideoChatRoom-simplified/js/video_chat_room.js
@@ -3,8 +3,9 @@ var video_out = document.getElementById("vid-box");
 function login(form) {
 	var phone = window.phone = PHONE({
 	    number        : form.username.value || "Anonymous", // listen on username line else Anonymous
-	    publish_key   : 'pub-c-561a7378-fa06-4c50-a331-5c0056d0163c', // Your Pub Key
-	    subscribe_key : 'sub-c-17b7db8a-3915-11e4-9868-02ee2ddab7fe', // Your Sub Key
+	    publish_key   : 'pub-c-71ef7cc3-cbba-4681-a7a0-0f04c90d2ece', // Your Pub Key
+	    subscribe_key : 'sub-c-baf5124e-b619-11e6-b697-0619f8945a4f', // Your Sub Key
+	    ssl: true
 	});	
 	phone.ready(function(){form.username.style.background="#55ff5b"; form.login_submit.hidden="true"; });
 	phone.receive(function(session){
