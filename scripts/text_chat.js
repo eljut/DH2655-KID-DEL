25be616a1d1ee134e4a8366efc539945b195e744
Mattias Lönnerberg <mattias.lonnerberg@gmail.com>
2016-11-30 15:40:14 +0100

    Chat and stuff
    Might work...

---
+++ scripts/text_chat.js
@@ -0,0 +1,50 @@
+
+var pubnub = PUBNUB.init({
+    publish_key: 'pub-c-71ef7cc3-cbba-4681-a7a0-0f04c90d2ece',
+    subscribe_key: 'sub-c-baf5124e-b619-11e6-b697-0619f8945a4f',
+    error: function (error) {
+        console.log('Error:', error);
+    }
+});
+console.log('pubnub initialized');
+
+pubnub.subscribe({
+    channel : 'public_channel',
+    message : function(m){
+        showChatOutput(m)
+    },
+    error : function (error) {
+        // Handle error here
+        console.log(JSON.stringify(error));
+    }
+});
+
+document.getElementById("pubnub-chat-input")
+	.addEventListener("keyup", function(event) {
+		event.preventDefault();
+		if (event.keyCode == 13) {
+			publishMessage();
+		}
+	});
+	
+function publishMessage() {
+	var chat_in = document.getElementById("pubnub-chat-input");
+	pubnub.publish({
+		channel : 'public_channel',
+		message : chat_in.value
+	});
+	console.log('message published: ' + chat_in.value);
+	// for testing.
+	/*var chat_out = document.getElementById("pubnub-chat-output");
+  var newchat = document.createElement('div');
+	newchat.innerHTML = chat_in.value;
+  chat_out.insertBefore( newchat, chat_out.firstChild );
+	chat_in.value = '';*/
+}
+
+function showChatOutput(m) {
+	var chat_out = document.getElementById("pubnub-chat-output");
+  var newchat = document.createElement('div');
+	newChat.innerHTML = m;
+  chat_out.insertBefore( newchat, chat_out.firstChild );
+}
