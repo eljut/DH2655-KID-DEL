d95184f092e2627ea78bd63cda09b6351f58602f
Daniel Gustavsson <danielgu@kth.se>
2016-12-01 01:37:10 +0100

    mkay
    lets go! :D

--- VideoChatRoom-simplified/js/video_chat_room.js
+++ VideoChatRoom-simplified/js/video_chat_room.js
@@ -5,8 +5,9 @@ function login(form) {
 	    number        : form.username.value || "Anonymous", // listen on username line else Anonymous
 	    publish_key   : 'pub-c-71ef7cc3-cbba-4681-a7a0-0f04c90d2ece', // Your Pub Key
 	    subscribe_key : 'sub-c-baf5124e-b619-11e6-b697-0619f8945a4f', // Your Sub Key
+			secretKey: 'sec-c-Nzc4MzgzYWQtMjMwYS00NWUzLWI1MzQtMjZhNTIzMGFhMTNh'
 	    ssl: true
-	});	
+	});
 	phone.ready(function(){form.username.style.background="#55ff5b"; form.login_submit.hidden="true"; });
 	phone.receive(function(session){
 	    session.connected(function(session) { video_out.appendChild(session.video); showModal();});
@@ -14,7 +15,7 @@ function login(form) {
 	});
 	return false;
 }
-    
+
 function makeCall(form){
 	if (!window.phone) alert("Login First!");
 	else phone.dial(form.number.value);
