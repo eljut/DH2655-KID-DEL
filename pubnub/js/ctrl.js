608f2126d465f4d2d562eeaf6cec09038037fd4b
Daniel Gustavsson <danielgu@kth.se>
2016-12-06 00:21:15 +0100

    hahah
    fml osv!

--- pubnub/js/ctrl.js
+++ pubnub/js/ctrl.js
@@ -15,4 +15,24 @@ $(document).ready(function(){
     $('#profile').show();
   });
 
+  $( "select" ).change(function(){
+    $('#number').val(
+      $('select').val()
+    )
+  });
+
+  function maxWin(){
+    var i = document.getElementById("video-display");
+
+    // go full-screen
+    if (i.requestFullscreen) {
+      i.requestFullscreen();
+    } else if (i.webkitRequestFullscreen) {
+      i.webkitRequestFullscreen();
+    } else if (i.mozRequestFullScreen) {
+      i.mozRequestFullScreen();
+    } else if (i.msRequestFullscreen) {
+      i.msRequestFullscreen();
+    }
+  }
 });
