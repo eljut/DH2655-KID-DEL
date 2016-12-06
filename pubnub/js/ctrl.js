f777a2a5a04eaa2eb069b6be64360a2c0950dda4
Daniel Gustavsson <danielgu@kth.se>
2016-12-06 01:07:17 +0100

    mdf
    mdf

--- pubnub/js/ctrl.js
+++ pubnub/js/ctrl.js
@@ -20,19 +20,6 @@ $(document).ready(function(){
       $('select').val()
     )
   });
-
-  function maxWin(){
-    var i = document.getElementById("video-display");
-
-    // go full-screen
-    if (i.requestFullscreen) {
-      i.requestFullscreen();
-    } else if (i.webkitRequestFullscreen) {
-      i.webkitRequestFullscreen();
-    } else if (i.mozRequestFullScreen) {
-      i.mozRequestFullScreen();
-    } else if (i.msRequestFullscreen) {
-      i.msRequestFullscreen();
-    }
-  }
 });
+
+ // Version 1.0
